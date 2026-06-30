48111ac7c184600001cbbee896e3e6e3bff7826c
monilpatelb26-sjp <monilpatelb31@gmail.com>
2026-06-30 22:32:25 +0530

    Remove conflicting @CrossOrigin annotations from controllers

--- backend/dsamaster/src/main/java/com/monil/dsamaster/controller/ProblemController.java
+++ backend/dsamaster/src/main/java/com/monil/dsamaster/controller/ProblemController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+
 @RestController
 @RequestMapping("/api/problems")
 public class ProblemController {
--- backend/dsamaster/src/main/java/com/monil/dsamaster/controller/RevisionController.java
+++ backend/dsamaster/src/main/java/com/monil/dsamaster/controller/RevisionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+
 @RestController
 @RequestMapping("/api/revisions")
 public class RevisionController {
